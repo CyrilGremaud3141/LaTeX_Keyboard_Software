23474e1bae10dff4d8ff66f134a898d5ecc58ca8
CyrilGremaud <64592319+CyrilGremaud3141@users.noreply.github.com>
2022-07-18 00:31:26 +0200

    working conversion between latex formula string and image

---
+++ rendering/latex_to_image.py
@@ -0,0 +1,41 @@
+import os
+
+"""
+Generiert ein auf dem Display darstellbares Bild aus dem generierten Latex string.
+"""
+class LatexToImage:
+    def __init__(self):
+        self.filepath = "conversion_file"
+        self.quality = 1000
+    
+    # schreibt den string in ein Latex file
+    def write_to_latex_file(self, tex_string):
+        # oeffnet das Latex file und trennt den Formel Teil heraus (Teil zwischen $ und $)
+        with open(f"{self.filepath}.tex", "r") as f:
+            content = f.read()
+            begin, _, end = content.split("$")
+        # schreibt den Latex string in das Dokument und behaelt Anfang und Ende bei.
+        with open(f"{self.filepath}.tex", "w") as f:
+            f.writelines(f"{begin}${tex_string}${end}")
+
+    # erstellt ein Bild aus dem Latex file
+    def convert(self):
+        # fuehrt einen Terminal Befehl aug um ein Pdf aus dem Tex file zu erstellen
+        os.system(f"pdflatex {self.filepath}.tex")
+        # erstellt mit einem Terminal Befehl ein Bild aus dem Pdf
+        os.system(f"pdftoppm {self.filepath}.pdf {self.filepath} -png -r {self.quality}")
+
+    # erstellt das Bild und gibt es zurueck
+    def make_image(self, tex_string):
+        self.write_to_latex_file(tex_string)
+        self.convert()
+        # das Bild muss noch im richtigen Format zur Darstellung eingelesen werden.
+        image = None
+        return image
+
+
+
+if __name__ == "__main__":
+    tex2png = LatexToImage()
+    tex2png.write_to_latex_file("\\alpha \\beta")
+    tex2png.convert()
