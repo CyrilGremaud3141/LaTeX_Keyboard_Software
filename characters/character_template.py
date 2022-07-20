c2f6838a7f35bd569825776ccee9f776839c7988
CyrilGremaud <64592319+CyrilGremaud3141@users.noreply.github.com>
2022-07-20 14:17:36 +0200

    character template created

---
+++ characters/character_template.py
@@ -0,0 +1,14 @@
+class Character:
+    cursor_order = [0, 1]
+    def __init__(self):
+        self.sub_elements = {
+            0 : [],
+            1 : [],
+        }
+        self.cursor_position = 0
+
+    def get_latex(self):
+        pass
+
+    def get_result(self):
+        pass
